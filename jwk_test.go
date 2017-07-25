ff3439426912c4c822d1e227fac3d6c8f6fb3725
Rahul Patel <rahul.patel@scaleft.com>
2017-07-25 10:05:05 -0700

    Fixing indentation

--- jwk_test.go
+++ jwk_test.go
@@ -402,8 +402,8 @@ var cookbookJWKs = []string{
 	stripWhitespace(`{
 	 "kty": "OKP",
 	 "crv": "Ed25519",
-     "d": "nWGxne_9WmC6hEr0kuwsxERJxWl7MmkZcDusAxyuf2A",
-     "x": "11qYAYKxCrfVS_7TyWQHOg7hcvPapiMlrwIaaPcHURo"
+      "d": "nWGxne_9WmC6hEr0kuwsxERJxWl7MmkZcDusAxyuf2A",
+      "x": "11qYAYKxCrfVS_7TyWQHOg7hcvPapiMlrwIaaPcHURo"
    }`),
 
 	// EC Private
