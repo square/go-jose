4b3c0f55ff8dad70cbc468ab3efda03b403052c8
Cedric Staub <cs@squareup.com>
2019-06-03 10:21:04 -0700

    Add comment explaining padding

--- cipher/ecdh_es.go
+++ cipher/ecdh_es.go
@@ -49,6 +49,10 @@ func DeriveECDHES(alg string, apuData, apvData []byte, priv *ecdsa.PrivateKey, p
 	z, _ := priv.Curve.ScalarMult(pub.X, pub.Y, priv.D.Bytes())
 	zBytes := z.Bytes()
 
+	// Note that calling z.Bytes() on a big.Int may strip leading zero bytes from
+	// the returned byte array. This can lead to a problem where zBytes will be
+	// shorter than expected which breaks the key derivation. Therefore we must pad
+	// to the full length of the expected coordinate here before calling the KDF.
 	octSize := dSize(priv.Curve)
 	if len(zBytes) != octSize {
 		zBytes = append(bytes.Repeat([]byte{0}, octSize-len(zBytes)), zBytes...)
