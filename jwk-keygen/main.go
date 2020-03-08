6ed2107f38ea7e1e730fbc0b3d84dafc2b0d8e49
Mat Byczkowski <mbyczkowski@squareup.com>
2020-03-07 17:32:04 -0800

    added error checking avoid nil referencing when error occurs
    Cherry-picked from #250
    
    Since jwk-keygen/main.go was moved to jose-util/generate.go on master,
    this required the patch to be applied manually.
    
    Co-authored-by: Trung Nguyen <trung.n.k@gmail.com>

--- jwk-keygen/main.go
+++ jwk-keygen/main.go
@@ -25,9 +25,10 @@ import (
 	"encoding/base32"
 	"errors"
 	"fmt"
-	"golang.org/x/crypto/ed25519"
 	"io"
 	"os"
+
+	"golang.org/x/crypto/ed25519"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 	"gopkg.in/square/go-jose.v2"
@@ -75,20 +76,32 @@ func KeygenSig(alg jose.SignatureAlgorithm, bits int) (crypto.PublicKey, crypto.
 	case jose.ES256:
 		// The cryptographic operations are implemented using constant-time algorithms.
 		key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		if err != nil {
+			return nil, nil, err
+		}
 		return key.Public(), key, err
 	case jose.ES384:
 		// NB: The cryptographic operations do not use constant-time algorithms.
 		key, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+		if err != nil {
+			return nil, nil, err
+		}
 		return key.Public(), key, err
 	case jose.ES512:
 		// NB: The cryptographic operations do not use constant-time algorithms.
 		key, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+		if err != nil {
+			return nil, nil, err
+		}
 		return key.Public(), key, err
 	case jose.EdDSA:
 		pub, key, err := ed25519.GenerateKey(rand.Reader)
 		return pub, key, err
 	case jose.RS256, jose.RS384, jose.RS512, jose.PS256, jose.PS384, jose.PS512:
 		key, err := rsa.GenerateKey(rand.Reader, bits)
+		if err != nil {
+			return nil, nil, err
+		}
 		return key.Public(), key, err
 	default:
 		return nil, nil, errors.New("unknown `alg` for `use` = `sig`")
@@ -106,6 +119,9 @@ func KeygenEnc(alg jose.KeyAlgorithm, bits int) (crypto.PublicKey, crypto.Privat
 			return nil, nil, errors.New("too short key for RSA `alg`, 2048+ is required")
 		}
 		key, err := rsa.GenerateKey(rand.Reader, bits)
+		if err != nil {
+			return nil, nil, err
+		}
 		return key.Public(), key, err
 	case jose.ECDH_ES, jose.ECDH_ES_A128KW, jose.ECDH_ES_A192KW, jose.ECDH_ES_A256KW:
 		var crv elliptic.Curve
@@ -120,6 +136,9 @@ func KeygenEnc(alg jose.KeyAlgorithm, bits int) (crypto.PublicKey, crypto.Privat
 			return nil, nil, errors.New("unknown elliptic curve bit length, use one of 256, 384, 521")
 		}
 		key, err := ecdsa.GenerateKey(crv, rand.Reader)
+		if err != nil {
+			return nil, nil, err
+		}
 		return key.Public(), key, err
 	default:
 		return nil, nil, errors.New("unknown `alg` for `use` = `enc`")
